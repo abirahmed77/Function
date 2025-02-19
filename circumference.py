6c124cb8ac3caa5ab4b5114f3a5aa91d5a237c35
Abir <abirgpx@gmail.com>
2025-02-19 16:48:27 +0600

    hi

--- circumference.py
+++ circumference.py
@@ -2,9 +2,12 @@ def circumference(r):
     return(2*3.1416*r) 
 
 def area(r):
-    return(3.1416*r)
+    return(3.1416*r*r)
 
 radius = float(input("Enter the Radius : "))
 
-circumference()
-area()
+cir = circumference(radius)
+ar = area(radius)
+
+print(f"Area is: {ar}")
+print(f"Circumference is: {cir}")
